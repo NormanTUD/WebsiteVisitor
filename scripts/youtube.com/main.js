789b83a83ac4e0860ba9f8bdc57401215ed4a229
n <norman.koch@tu-dresden.de>
2025-06-12 19:27:22 +0200

    fix

--- scripts/youtube.com/main.js
+++ scripts/youtube.com/main.js
@@ -1,12 +1,10 @@
 const intervalId = setInterval(() => {
-  // Suche den Button anhand der Klasse und des Texts "Accept all"
   const buttons = document.querySelectorAll('button.yt-spec-button-shape-next');
   for (let btn of buttons) {
-    // Sicherstellen, dass es der richtige Button mit Text "Accept all" ist
     if (btn.textContent.trim() === 'Accept all') {
       btn.click();
-      clearInterval(intervalId); // Intervall stoppen, wenn Button geklickt
+      clearInterval(intervalId);
       break;
     }
   }
-}, 500);
+}, 1500);
