e5551a0c4203b6144bd1896fb18953c08ca1eb4b
n <norman.koch@tu-dresden.de>
2025-06-12 20:00:33 +0200

    fix

--- scripts/soundcloud.com/main.js
+++ scripts/soundcloud.com/main.js
@@ -1,2 +1,31 @@
-// Suche den großen Playbutton und klicke ihn an
-document.getElementsByClassName("sc-button-xxlarge")[0].click()
+function waitForElement(selector, description, callback) {
+    updateStatus("Warte auf '" + selector + "' (" + description + ")...");
+    let interval = setInterval(() => {
+        let el = document.querySelector(selector);
+        if (el && el.offsetParent !== null) {
+            updateStatus("Gefunden: '" + selector + "', " + description + "...");
+            clearInterval(interval);
+            callback(el);
+        }
+    }, 500);
+}
+
+function waitForConsentAndClickNext() {
+    waitForElement(".onetrust-accept-btn-handler", "Cookie-Zustimmung", (consentBtn) => {
+        consentBtn.click();
+        updateStatus("Cookie-Zustimmung geklickt.");
+
+        waitForElement(".sc-button-xxlarge", "großer Playbutton", (playBtn) => {
+            playBtn.click();
+            updateStatus("Playbutton geklickt.");
+        });
+    });
+}
+
+waitForConsentAndClickNext();
+
+updateStatus("Suche den grossen Playbutton");
+let playbutton = document.getElementsByClassName("sc-button-xxlarge")[0];
+
+updateStatus("Klicke den grossen Playbutton an")
+playbutton.click()
