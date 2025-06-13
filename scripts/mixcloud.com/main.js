af1173066db2e56ec765a5602115f3b1d1ed1c81
n <norman.koch@tu-dresden.de>
2025-06-13 12:59:54 +0200

    isVisible

--- scripts/mixcloud.com/main.js
+++ scripts/mixcloud.com/main.js
@@ -1,19 +1,39 @@
-function click_random_play_button() {
-	var buttons = document.querySelectorAll('[data-testid="audiocard-play-button"]');
+function isVisible(el) {
+        updateStatus("Check if element and all parents are visible");
 
-	if (buttons.length === 0) {
-		updateStatus('Keine Buttons gefunden.');
-	} else {
-		var index = Math.floor(Math.random() * buttons.length);
-		var button = buttons[index];
-		updateStatus('Wähle zufällig Button #' + (index + 1) + ' von ' + buttons.length);
-		button.click();
-	}
+        while (el) {
+                const style = window.getComputedStyle(el);
+                if (style.display === 'none' || style.visibility === 'hidden' || style.opacity === '0') {
+                        return false;
+                }
+                el = el.parentElement;
+        }
+        return true;
+}
+
+function click_random_play_button() {
+        var buttons = document.querySelectorAll('[data-testid="audiocard-play-button"]');
+
+        if (buttons.length === 0) {
+                updateStatus('Keine kleinen Buttons gefunden, versuche, grosse Buttons zu finden.');
+
+                buttons = Array.from(document.getElementsByClassName("play-button-rings-svg"));
+                buttons = buttons.filter(isVisible);
+        }
+
+        if(buttons.length) {
+                var index = Math.floor(Math.random() * buttons.length);
+                var button = buttons[index];
+                updateStatus('Wähle zufällig Button #' + (index + 1) + ' von ' + buttons.length);
+                button.click();
+        } else {
+                updateStatus("Keine Buttons gefunden");
+        }
 }
 
 async function run_script() {
-	await sleepRandomly(6000, 8000);
-	click_random_play_button()
+        await sleepRandomly(6000, 8000);
+        click_random_play_button()
 }
 
 run_script();
